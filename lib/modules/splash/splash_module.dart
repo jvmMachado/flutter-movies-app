55599e4cb95cb84ce331f2f9c6c91a1b5b11aa22
jvmMachado <joaovmmachado@gmail.com>
2022-01-12 19:23:39 -0300

    changing from extends to implements due to module being an abstract class

--- lib/modules/splash/splash_module.dart
+++ lib/modules/splash/splash_module.dart
@@ -2,7 +2,7 @@ import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:movies_app/application/modules/module.dart';
 import 'package:movies_app/modules/splash/splash_page.dart';
 
-class SplashModule extends Module {
+class SplashModule implements Module {
   @override
-  List<GetPage> routers = [GetPage(name: '/', page: () => SplashPage())];
+  List<GetPage> routers = [GetPage(name: '/', page: () => const SplashPage())];
 }
