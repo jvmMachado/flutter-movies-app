b851519fbc86a53ceb31e1c370e6eeb4e46e3f73
jvmMachado <joaovmmachado@gmail.com>
2022-01-16 01:45:06 -0300

    create splash controller and bindings

--- lib/modules/splash/splash_module.dart
+++ lib/modules/splash/splash_module.dart
@@ -1,8 +1,12 @@
 import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:movies_app/application/modules/module.dart';
+import 'package:movies_app/modules/splash/splash_bindings.dart';
 import 'package:movies_app/modules/splash/splash_page.dart';
 
 class SplashModule implements Module {
   @override
-  List<GetPage> routers = [GetPage(name: '/', page: () => const SplashPage())];
+  List<GetPage> routers = [
+    GetPage(
+        name: '/', page: () => const SplashPage(), binding: SplashBindings())
+  ];
 }
