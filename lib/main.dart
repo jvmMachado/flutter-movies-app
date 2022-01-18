cc85bc46f4f6637203d6a21592bbad0803679cc0
jvmMachado <joaovmmachado@gmail.com>
2022-01-18 09:46:58 -0300

    edit login domain

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get/route_manager.dart';
 import 'package:movies_app/application/bindings/application_bindings.dart';
+import 'package:movies_app/modules/home/home_module.dart';
 import 'package:movies_app/modules/login/login_module.dart';
 
+import 'application/ui/movies_app_ui_config.dart';
 import 'modules/splash/splash_module.dart';
 
 import 'package:firebase_core/firebase_core.dart';
@@ -27,14 +29,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: MoviesAppUiConfig.title,
       initialBinding: ApplicationBindings(),
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: MoviesAppUiConfig.theme,
       getPages: [
         ...SplashModule().routers,
         ...LoginModule().routers,
+        ...HomeModule().routers,
       ],
     );
   }
