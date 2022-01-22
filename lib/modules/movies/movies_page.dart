70b49646f3d70c463788a6552cde7c39963a0302
jvmMachado <joaovmmachado@gmail.com>
2022-01-22 16:14:24 -0300

    create and add movies header to movies page

--- lib/modules/movies/movies_page.dart
+++ lib/modules/movies/movies_page.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:movies_app/modules/movies/widgets/movies_header.dart';
 
 class MoviesPage extends StatelessWidget {
   const MoviesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('Movies Page'),
-    );
+    return SizedBox(
+        width: Get.width,
+        child: ListView(
+          children: [
+            MoviesHeader(),
+          ],
+        ));
   }
 }
