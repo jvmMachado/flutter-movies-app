5d709af0b316724c3a310d25d13773f9ed290694
jvmMachado <joaovmmachado@gmail.com>
2022-01-29 17:21:18 -0300

    create MovieDetailsContent widget

---
+++ lib/modules/movie_detail/widgets/movie_details_content.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MovieDetailsContent extends StatelessWidget {
+  const MovieDetailsContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Container(),
+    );
+  }
+}
